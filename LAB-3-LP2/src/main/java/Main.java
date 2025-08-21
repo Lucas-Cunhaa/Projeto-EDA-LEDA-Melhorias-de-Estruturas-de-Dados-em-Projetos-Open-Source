cdf2826a9f39645e4a2e3bc7beec1867a4e53291
lucascunha <lucasgalvao260@gmail.com>
2025-08-21 20:50:03 -0300

    [LAB-3-LP2] feat: added the README.md

--- LAB-3-LP2/src/main/java/Main.java
+++ LAB-3-LP2/src/main/java/Main.java
@@ -1,5 +1,32 @@
+package main.java;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 public class Main {
     public static void main(String[] args) {
-        
+          try {
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	            String line = "";
+	            // Cabeçalho
+	            System.out.println("Method Time Sample");
+	            while ((line = reader.readLine()) != null) {
+	                String[] tokens = line.split(" ");
+                    FilmNow fm = new FilmNow();
+	                // medindo tempo de execução 
+	                long start = System.nanoTime();
+	                
+	                // TODO incluir aqui chamada para o método sob análise
+	                
+	                System.out.println(fm);
+	                long end = System.nanoTime();
+	                long time = end - start;
+	                
+	                // saída padrão: método tempo tamanho_da_entrada
+	                System.out.println("Cadastra Filmes " + (time) + " " + tokens.length);
+
+	            }
+	        } catch (IOException ioe) {}
     }
 }
