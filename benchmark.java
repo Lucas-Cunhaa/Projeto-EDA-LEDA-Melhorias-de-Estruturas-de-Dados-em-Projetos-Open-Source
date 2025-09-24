9b7d75ebff2589ed8c7af134d076d908d55e7050
caiosimpl <caio1simplicio@gmail.com>
2025-09-24 12:02:29 -0300

    adding benchmark

---
+++ benchmark.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class benchmark {
+    
+
+    public static void main(String[] args) {
+        
+        ArrayList<HashMap<String, String>> mapMoves = new ArrayList<HashMap<String, String>>();
+
+    }
+
+}
