7f0409e0b46da7c6e3cb316d6ac4f285ff3e8978
leticialunaz <leticialunadb@gmail.com>
2025-09-26 19:33:56 -0300

    organizando pastas

---
+++ Letícia-Hangman/script/plot.R
@@ -0,0 +1,11 @@
+# Se o pacote não tiver instalado na máquina.
+#install.packages('ggplot2', repos = "http://cran.us.r-project.org")
+
+library('ggplot2')
+
+args <- commandArgs()
+
+data = read.table(args[length(args)], header = T)
+ggplot(data, aes(x = Sample, y = Time, colour = Method)) + geom_line()
+
+
